c2a19e82ce020d6efd981e1ded74c30958b907ba
ubaidullahshahid <ubaidullahshahid098@gmail.com>
2024-05-10 02:24:29 +0500

    solve routing issues

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ function logIn() {
         userLogIn = true;
         if (userLogIn) {
           localStorage.setItem("userLogin", JSON.stringify(userLogIn));
-          window.location.href = "/index.html";
+          window.location.href = "index.html";
         }
       } else {
         alert("Invalid email or password");
